d9d139d88ac1b7277493442f856f901e2a99ed44
naahthy <109962623+naahthy@users.noreply.github.com>
2023-09-11 11:52:58 -0300

    ;

---
+++ main.js
@@ -0,0 +1,10 @@
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play()
+}
+
+const listaDeTeclas= document.querySelectorAII('.tecla');
+
+//para
+for (let contador= 0; contador < listaDeTeclas.length;contador++){
+    
+}
